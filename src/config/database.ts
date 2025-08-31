0d9ee75dc20789e0fe9ad060ab5bc697a0274b43
kavin-isteer <kavinkr.isteer.com>
2025-08-31 19:20:57 +0530

    added ssl true in dialect options

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,20 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 const dbUsername = process.env.DB_USER || '';
 const dbPassword = process.env.DB_PASSWORD || '';
 const dbHost = process.env.DB_HOST || '';
 const dbName = process.env.DB_NAME || '';
 
-const sequelize = new Sequelize(dbName,dbUsername,dbPassword,
-{
-    host:dbHost,
-    dialect:'postgres'
-});
+const sequelize = new Sequelize(dbName, dbUsername, dbPassword,
+    {
+        host: dbHost,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // for Render; set true in secure envs
+            }
+        }
+    });
 
 export default sequelize;
